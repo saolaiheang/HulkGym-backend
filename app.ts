b9ea89c0cdc9868792af908c4e0a742ad465a507
sreypheaphorn <sreyphea.phorn@institute.pse.ngo>
2025-02-18 14:33:23 +0700

    feature coupon

--- app.ts
+++ app.ts
@@ -12,6 +12,8 @@ import activity from "./src/routes/activity";
 import telegramBot from "node-telegram-bot-api";
 import { handleMessage } from "./src/service/telegram.service";
 import Promotion from "./src/routes/promotion";
+import coupon from "./src/routes/coupon"
+
 import axios from "axios";
 
 // replace the value below with the Telegram token you receive from @BotFather
@@ -36,6 +38,8 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use("/api/auth", auth);
 app.use("/api/activity", activity);
 app.use("/api/promotion", Promotion);
+app.use("/api/coupon",coupon)
+
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new telegramBot(token, { polling: true });
