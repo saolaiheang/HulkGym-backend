c9bd4ae20f3eb909a884983b9b1beccbbbece8c1
sreypheaphorn <sreyphea.phorn@institute.pse.ngo>
2025-02-28 07:53:59 +0700

    join us

--- src/entity/user.entity.ts
+++ src/entity/user.entity.ts
@@ -1,3 +1,55 @@
+// import "reflect-metadata";
+// import {
+//   Entity,
+//   PrimaryGeneratedColumn,
+//   Column,
+//   CreateDateColumn,
+//   UpdateDateColumn,
+//   OneToMany,
+//   OneToOne,
+// } from "typeorm";
+// import { RoleEnum } from "../common/types/enum";
+// import { Activity } from "./activity.entity";
+// import { Branch_User } from "./branch-user.entity";
+
+// @Entity({ name: "user_info" })
+// export class UserInfo {
+//   @PrimaryGeneratedColumn("uuid")
+//   id: string;
+
+//   @Column({ nullable: false })
+//   name: string;
+
+//   @Column({ nullable: false })
+//   userEmail: string;
+
+//   @Column({ nullable: true })
+//   userContact: string;
+
+//   @Column({ nullable: false })
+//   password: string;
+
+//   @Column({
+//     type: "varchar",
+//     length: 255,
+//     nullable: true,
+//     default: RoleEnum[2],
+//   }) // Fix here
+//   role: string;
+
+//   @CreateDateColumn()
+//   createdAt: Date;
+
+//   @UpdateDateColumn()
+//   modifiedAt: Date;
+
+//   @OneToMany(() => Activity, (activity) => activity.user)
+//   activities: Activity[]; // One user can have many activities
+
+//   @OneToMany(() => Branch_User, (branch_user) => branch_user.user)
+//   branch_users: Branch_User[];
+// }
+
 import "reflect-metadata";
 import {
   Entity,
@@ -6,11 +58,14 @@ import {
   CreateDateColumn,
   UpdateDateColumn,
   OneToMany,
+  ManyToOne,
+  JoinColumn,
   OneToOne,
 } from "typeorm";
 import { RoleEnum } from "../common/types/enum";
 import { Activity } from "./activity.entity";
 import { Branch_User } from "./branch-user.entity";
+import { Coupon } from "./coupon.entity";
 
 @Entity({ name: "user_info" })
 export class UserInfo {
@@ -18,23 +73,26 @@ export class UserInfo {
   id: string;
 
   @Column({ nullable: false })
-  name: string;
+  first: string;
 
   @Column({ nullable: false })
-  userEmail: string;
-
-  @Column({ nullable: true })
-  userContact: string;
+  last: string;
 
   @Column({ nullable: false })
-  password: string;
+  email: string;
+
+  @Column({ nullable: false })
+  phone: number;
+  
+  @OneToOne(() => Coupon, (coupon) => coupon.user, { nullable: true })
+  coupon: Coupon[];
 
   @Column({
     type: "varchar",
     length: 255,
     nullable: true,
     default: RoleEnum[2],
-  }) // Fix here
+  })
   role: string;
 
   @CreateDateColumn()
@@ -44,7 +102,7 @@ export class UserInfo {
   modifiedAt: Date;
 
   @OneToMany(() => Activity, (activity) => activity.user)
-  activities: Activity[]; // One user can have many activities
+  activities: Activity[];
 
   @OneToMany(() => Branch_User, (branch_user) => branch_user.user)
   branch_users: Branch_User[];
