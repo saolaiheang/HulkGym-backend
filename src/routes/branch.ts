ba8a39c46ece15da6df641f7258de23639210116
saolaiheang <laiheang.sao@institute.pse.ngo>
2025-03-05 17:01:18 +0700

    finall

--- src/routes/branch.ts
+++ src/routes/branch.ts
@@ -1,15 +1,15 @@
 import { Router } from "express";
 import { getBranch } from "../controllers/branch.controller";
-import { getById } from "../controllers/branch.controller";
-import { deleteById } from "../controllers/branch.controller";
-import { addBranch } from "../controllers/branch.controller";
+// import { getById } from "../controllers/branch.controller";
+// import { deleteById } from "../controllers/branch.controller";
+// import { addBranch } from "../controllers/branch.controller";
 const router = Router();
 
 
-router.post("/new", addBranch)
+// router.post("/new", addBranch)
 router.get("/all", getBranch)
-router.get("/:id", getById)
-router.delete("/:id", deleteById)
+// router.get("/:id", getById)
+// router.delete("/:id", deleteById)
 
 
 export default router;
